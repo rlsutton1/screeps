5a802c6afafdc8c0e8486a4aa2a363da1c7d87a0
Robert Sutton <rsutton@noojee.com.au>
2017-09-20 13:21:34 +1000

    fix spawn message

--- src/roomController.js
+++ src/roomController.js
@@ -102,7 +102,7 @@ run:function (room,mySettings) {
             if(spawn.spawning) { 
                 var spawningCreep = Game.creeps[spawn.spawning.name];
                 spawn.room.visual.text(
-                    '🛠️' + spawningCreep.memory.role,
+                     spawningCreep.memory.role,
                     spawn.pos.x + 1, 
                     spawn.pos.y, 
                     {align: 'left', opacity: 0.8});
