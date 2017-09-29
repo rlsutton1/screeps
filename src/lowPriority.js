339db89fb840fb65f70256eb930e458f9cfc0e5d
Robert Sutton <rsutton@noojee.com.au>
2017-09-29 13:20:36 +1000

    change minimum radius

--- src/lowPriority.js
+++ src/lowPriority.js
@@ -50,15 +50,17 @@ module.exports = {
 			if (spawn !=null){
 				var pos = spawn.pos;
 				var ctr = 0;
-				for (var radius = 3; radius <= 20;radius++)
+				for (var radius = 1; radius <= 20;radius++)
 					for (var x = -radius; x <= radius;x++)
 						for (var y = -radius; y <= radius;y++)
 						{
 							if (createMoreExtensions(ctr, extensions, exts, room)) {
 								return;
 							}
-							ctr += createExtension(room, pos.x + x, pos.y + y,
+							if (Math.abs(x) > 1 && Math.abs(y) > 1) {
+								ctr += createExtension(room, pos.x + x, pos.y + y,
 									utils);
+							}
 						}
 			}
 		}
